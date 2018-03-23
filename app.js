0f02f11feacfc11bd000927fecea34c878e8bb85
linberon <linberon@gmail.com>
2018-03-23 00:17:19 +0000

    precise rounding+background fix

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-function updateStats (){
+function updateStats() {
 	event.preventDefault();
-	var text = $ ('#user-text').val();
-	var words = text.split(' ');
+	var text = $("#user-text").val();
+	var words = text.split(" ");
 	var count = words.length;
-	$('#word-count').html(count);
+	$("#word-count").html(count);
 	var avg = averageWordLength(words);
-	$('#average-word').html(avg);
-    $('#unique-word').html(uniqueWords(words))
-};
+	$("#average-word").html(avg);
+	$("#unique-word").html(uniqueWords(words));
+}
 
 function averageWordLength(words) {
 	var total = 0;
-    for (i=0; i<words.length; i++){
-    	total = total + words[i].length
-    }
-    return (total/words.length);
+	for (i = 0; i < words.length; i++) {
+		total = total + words[i].length;
+	}
+	return (total / words.length).toFixed(2);
 }
 
 function uniqueWords(words) {
 	var unique = 0;
 	var seen = {};
-	for (i=0; i<words.length; i++){
+	for (i = 0; i < words.length; i++) {
 		var curWord = words[i];
-		if (seen[curWord] !== true){
-
+		if (seen[curWord] !== true) {
 			unique = unique + 1;
 		}
 		seen[curWord] = true;
 	}
-	return unique
+	return unique;
 }
 
-$('#update').submit(updateStats);
+$("#update").submit(updateStats);
